f08333e8ca5071100f7e6bca6f10d999fccad7d7
carogullon <gulloncarolina@gmail.com>
2021-09-01 11:36:30 -0300

    mejorando el formulario en mi entrega final

--- js/checkoutCarrito.js
+++ js/checkoutCarrito.js
@@ -49,6 +49,7 @@ function sumarCarrito() {
 	return total
 }
 
+
 let botonPedido = $("#btn-realizarPedido");
 if (botonPedido !== null) {
 	botonPedido.click(respuestaClick);
@@ -58,122 +59,98 @@ function respuestaClick() {
 	formularioUsuario();
 }
 
-function formularioUsuario() {
-	let nombreCompra = "";
-	if (document.querySelector("#shippingAddress.first_name") !==  null) {
-		nombreCompra = document.querySelector("#shippingAddress.first_name").value;
-	}else {
-		(document.querySelector("#shippingAddress.first_name") === null);
-		let errorNombre = document.querySelector("#error-nombre");
-		errorNombre.innerHTML = `<p class="errorFormEnvio">Debes completar nombre</p>`	
-	}
-	
-	let apellidoCompra = "";
-	if (document.querySelector("#shippingAddress.last_name") !== null) {
-		apellidoCompra = document.querySelector("#shippingAddress.last_name").value;
-	}else {
-		(document.querySelector("#shippingAddress.last_name") === null);
-		let errorApellido = document.querySelector("#error-apellido");
-		errorApellido.innerHTML = `<p class="errorFormEnvio">Debes completar apellido</p>`	
-	}
+const formulario = document.getElementById('formularioCompra');
+const inputs = document.querySelectorAll('#formularioCompra input');
 
-	let mailCompra =  "";
-	if (document.querySelector("#contact.email")!== null) {
-		mailCompra = document.querySelector("#contact.email").value;
-	}else {
-		(document.querySelector("#shippingAddress.email") === null);
-		let errorEmail = document.querySelector("#error-email");
-		errorEmail.innerHTML = `<p class="errorFormEnvio">Debes completar email</p>`	
-	}
-
-
-	let telefonoCompra = "";
-	if (document.querySelector("#shippingAddress.phone")!== null){
-		telefonoCompra = document.querySelector("#shippingAddress.phone").value;
-	}else {
-		(document.querySelector("#shippingAddress.phone") === null);
-		let errorPhone = document.querySelector("#error-phone");
-		errorPhone.innerHTML = `<p class="errorFormEnvio">Debes completar telefono</p>`	
-	}
-
-	let direccionCompra = "";
-	if (document.querySelector("#shippingAddress.address")!== null) {
-		direccionCompra = document.querySelector("#shippingAddress.address").value;
-	}else {
-		(document.querySelector("#shippingAddress.address") === null);
-		let errorAddress = document.querySelector("#error-address");
-		errorAddress.innerHTML = `<p class="errorFormEnvio">Debes completar dirección</p>`	
-	}
-
-	let ciudadCompra = "";
-	if (document.querySelector("#shippingAddress.city")!== null) {
-		ciudadCompra = document.querySelector("#shippingAddress.city").value;
-	}else {
-		(document.querySelector("#shippingAddress.city") === null);
-		let errorCity = document.querySelector("#error-city");
-		errorCity.innerHTML = `<p class="errorFormEnvio">Debes completar ciudad</p>`	
-	}
-
-	let numeroCasa = "";
-	if (document.querySelector("#shippingAddress.number")!== null) {
-		numeroCasa = document.querySelector("#shippingAddress.number").value;
-	}else {
-		(document.querySelector("#shippingAddress.number") === null);
-		let errorNum = document.querySelector("#error-number");
-		errorNum.innerHTML = `<p class="errorFormEnvio">Debes completar número</p>`	
-	}
-	let numeroTarjeta = "";
-	if (document.querySelector("#payment.creditCard.cardNumber")!== null) {
-		numeroTarjeta = document.querySelector("#payment.creditCard.cardNumber").value;
-	}else {
-		(document.querySelector("#payment.creditCard.cardNumber") === null);
-		let errorCardNumber = document.querySelector("#error-number-tarjeta");
-		errorCardNumber.innerHTML = `<p class="errorFormEnvio">Debes completar el número de tarjeta</p>`	
-	}
-
-	let titularTarjeta = "";
-	if (document.querySelector("#payment.creditCard.cardHolderName")!== null) {
-		titularTarjeta = document.querySelector("#payment.creditCard.cardHolderName").value;
-	}else {
-		(document.querySelector("#payment.creditCard.cardHolderName") === null);
-		let errorCardName = document.querySelector("#error-titular-tarjeta");
-		errorCardName.innerHTML = `<p class="errorFormEnvio">Debes completar el titular de tarjeta</p>`	
-	}
-
-	let vencimTarjeta = "";
-	if (document.querySelector("#payment.creditCard.cardExpiration")!== null) {
-		vencimTarjeta = document.querySelector("#payment.creditCard.cardExpiration").value;
-	}else {
-		(document.querySelector("#payment.creditCard.cardExpiration") === null);
-		let errorCardExp = document.querySelector("#error-card-expiration");
-		errorCardExp.innerHTML = `<p class="errorFormEnvio">Debes completar el vencimiento de la tarjeta</p>`	
-	}
-
-
-	let cvv = "";
-	if (document.querySelector("#payment.creditCard.cardCvv")!== null) {
-		cvv = document.querySelector("#payment.creditCard.cardCvv").value;
-	}else {
-		(document.querySelector("#payment.creditCard.cardCvv") === null);
-		let errorCardCvv = document.querySelector("#error-card-cvv");
-		errorCardCvv.innerHTML = `<p class="errorFormEnvio">Debes completar el CVV de la tarjeta</p>`	
-	}
-
-	let cuotas = "";
-	if (document.querySelector("#payment.creditCard.cardInstallments")!== null) {
-		cuotas =document.querySelector("#payment.creditCard.cardInstallments").value;
-	}else {
-		(document.querySelector("#payment.creditCard.cardInsstallments") === null);
-		let errorCardIns = document.querySelector("#error-card-installments");
-		errorCardIns.innerHTML = `<p class="errorFormEnvio">Debes elegir cantidad de cuotas</p>`	
-	}
-	let pedido = "";
-	if (document.querySelector("#textarea-pedido")!== null) {
-		pedido = document.querySelector("#textarea-pedido").value;
-	}else {
-		(document.querySelector("#textarea-pedido") === null);
-		let errorPedido = document.querySelector("#error-pedido");
-		errorPedido.innerHTML = `<p class="errorFormEnvio">Debes completar los detalles de tu pedido</p>`	
-	}
-	console.log(nombreCompra, apellidoCompra, mailCompra, telefonoCompra, direccionCompra, ciudadCompra, numeroCasa, numeroTarjeta, titularTarjeta, vencimTarjeta, cvv, cuotas, pedido);
+const expresiones = {
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	ciudad: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	direccion: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	numero:  /^\d{7,14}$/, // 7 a 14 numeros.
+	email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^[0-9]+$/,
+	tarjeta: /^[0-9]{15,16}|(([0-9]{4}\s){3}[0-9]{3,4})$/,// 16 digitos o de 4 en 4 separados por espacios.
 }
+
+const campos = {
+	nombre: false,
+	ciudad: false,
+	direccion: false,
+	email: false,
+	numero: false,
+	telefono: false,
+	tarjeta: false,
+}
+
+const validarFormulario = (e) => {
+	switch (e.target.name) {
+		case "nombre":
+			validarCampo(expresiones.nombre, e.target, 'nombre');
+		break;
+		case "email":
+			validarCampo(expresiones.email, e.target, 'email');
+		break;
+		case "ciudad":
+			validarCampo(expresiones.ciudad, e.target, 'ciudad');
+		break;
+		case "numero":
+			validarCampo(expresiones.numero, e.target, 'numero');
+		break;
+		case "telefono":
+			validarCampo(expresiones.telefono, e.target, 'telefono');
+		break;
+		case "direccion":
+			validarCampo(expresiones.direccion, e.target, 'direccion');
+		break;
+		case "tarjeta":
+			validarCampo(expresiones.tarjeta, e.target, 'tarjeta');
+		break;
+	}
+}
+
+const validarCampo = (expresion, input, campo) => {
+	if(expresion.test(input.value)){
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		campos[campo] = true;
+	} else {
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+		campos[campo] = false;
+	}
+}
+
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', validarFormulario);
+	input.addEventListener('blur', validarFormulario);
+});
+
+formulario.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	const terminos = document.getElementById('terminos');
+	
+	if(campos.nombre && campos.email && campos.ciudad && campos.direccion && campos.telefono && campos.numero &&campos.tarjeta && terminos.checked ){
+		
+		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+		console.log (nombre, email, direccion, ciudad, telefono, numero, tarjeta);
+		
+		setTimeout(() => {
+			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+		}, 5000);
+
+		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
+			icono.classList.remove('formulario__grupo-correcto');
+		});
+
+	} else {
+		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+	}
+});
